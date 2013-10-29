012f6b06ffa2fcc221518a8f37c2018cb5c1db5c
Andrew Carpenter <andrew@audioinhd.com>
2013-10-28 22:05:54 -0400

    initial test, testing chainability

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
 (function ($) {
 
-    test("This is your first test", function () {
-        ok($.fn.jqueryVideoLightning.options, "options set up correctly");
+    test("empty", function () {
+      equal(1, 1, "1 equals 1");
     });
+//    test("options", function () {
+//        ok($.fn.jqueryVideoLightning.settings(), "options set up correctly");
+//        equal($.fn.jqueryVideoLightning.settings().videoId, "y-dQw4w9WgXcQ", "default video id is set properly");
+//    });
+//
+//    test("defaults", function () {
+//        ok($('.test-target').jqueryVideoLightning().settings(), "options set up correctly");
+//        equal($('.test-target').jqueryVideoLightning().settings().videoId, "y-dQw4w9WgXcQ", "default video id is set properly");
+//    });
 
     test("chainable", function () {
         ok($('.test-target').jqueryVideoLightning().addClass("testing"), "can be chained");
